0c8e92b173ee2663262032dcea531b4a48f82a28
filippocamillo5 <filippocamillo5@icloud.com>
2022-03-02 01:52:13 -0300

    praticando o "if"

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 10                                                                    
+console.log (`A velocidade do seu carro é ${vel} por hora`)
+if (vel > 60){
+    console.log ('Você foi MULTADO!')
+}
+console.log (`Dirija sempre usando cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = "br"
+console.log (`voce nasceu no ${país}.`)
+if (país == "eua"){
+    console.log (`se deu bem`)
+}else{
+    console.log (`fudida`)
+}
