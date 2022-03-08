204d4054b120c9b38bcc403f8e2b79b67303e6e4
filippocamillo5 <filippocamillo5@icloud.com>
2022-03-08 20:51:41 -0300

    atualização js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,6 +1,7 @@
 var num = [2, 3, 1, 900]
+num.push(100000)
+num[3] = 6
 console.log(`As varáveis de "num" são ${num}`)
 console.log(`As varáveis de "num" em ordem crescente são: ${num.sort()}`)
 console.log(`a array "num" contem ${num.length} variáveis`)
 console.log(`O primeir elemente deste array é: ${num[0]}`)
-num.push(1)
