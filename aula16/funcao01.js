075c27d26eae42ec6a06f8b20038b5c7d88aa5a4
filippocamillo5 <filippocamillo5@icloud.com>
2022-03-09 20:21:33 -0300

    funções

---
+++ aula16/funcao01.js
@@ -0,0 +1,15 @@
+function parimp(n) {
+    if (n%2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    } 
+}
+      
+
+console.log (parimp(5))
+
+let res = parimp(5)
+console.log(res)
+
+console.log (parimp(4))
